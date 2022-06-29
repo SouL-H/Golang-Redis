f0248496ed1209042e6076d2a1c4067dda7270fb
SouL-H <mhmtaydinw@gmail.com>
2022-06-29 11:01:51 +0300

    data to Struct append

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
+type Album struct {
+	Title  string  `redis:"title"`
+	Artist string  `redis:"creator"`
+	Price  float64 `redis:"price"`
+	Likes  int     `redis:"likes"`
+}
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -22,5 +29,20 @@ func main() {
 	title, err := redis.String(conn.Do("HGET", "album:2", "title"))
 	checkErr(err)
 	fmt.Println(title)
+	fee, err := redis.Float64(conn.Do("HGET", "album:2", "price"))
+	checkErr(err)
+	fmt.Println(fee)
+	values, err := redis.StringMap(conn.Do("HGETALL", "album:2"))
+	checkErr(err)
+	for k, v := range values {
+		fmt.Println("Key: ", k)
+		fmt.Println("Value: ", v)
+	}
+	reply, err := redis.Values(conn.Do("HGETALL", "album:2"))
+	checkErr(err)
+	var album Album
+	err = redis.ScanStruct(reply, &album)
+	checkErr(err)
+	fmt.Println(album)
 
 }
